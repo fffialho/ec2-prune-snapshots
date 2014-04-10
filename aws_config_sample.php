1a5c0f3d9dd9b8f27306aed7e0d8ba79aa64782c
Francisco Fialho <francisco@propus.com.br>
2014-04-10 17:25:06 -0300

    Adjusting output and testing exception indicating snapshot in use by a ami in order to continue execution in this case.

---
+++ aws_config_sample.php
@@ -0,0 +1,13 @@
+<?php return array(
+    'includes' => array('_aws'),
+    'services' => array(
+        'default_settings' => array(
+            'params' => array(
+                'key'    => 'YOUR_KEY',
+                'secret' => 'YOUR_SECRET',
+                'region' => 'us-east-1'
+            )
+        )
+    )
+);
+
